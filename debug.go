4ba628d907d16f176c615779cde86dad44b78d41
Patrick Chan <patrickchan1999@gmail.com>
2019-10-24 19:32:43 -0400

    Use the debugMode flag to see if debugging information should be printed

--- debug.go
+++ debug.go
@@ -10,24 +10,34 @@ func DebugError(debugMode bool, err error) {
 	if err == nil {
 		return
 	}
-	tm.Print(tm.Color(fmt.Sprintf("[DEBUG] %s", err.Error()), tm.RED))
-	tm.Flush()
+
+	if debugMode {
+		tm.Print(tm.Color(fmt.Sprintf("[DEBUG] %s", err.Error()), tm.RED))
+		tm.Flush()
+	}
 }
 
 func DebugErrorln(debugMode bool, err error) {
 	if err == nil {
 		return
 	}
-	tm.Println(tm.Color(fmt.Sprintf("[DEBUG] %s", err.Error()), tm.RED))
-	tm.Flush()
+
+	if debugMode {
+		tm.Println(tm.Color(fmt.Sprintf("[DEBUG] %s", err.Error()), tm.RED))
+		tm.Flush()
+	}
 }
 
 func DebugString(debugMode bool, s string, color int) {
-	tm.Print(tm.Color(fmt.Sprintf("[DEBUG] %s", s), color))
-	tm.Flush()
+	if debugMode {
+		tm.Print(tm.Color(fmt.Sprintf("[DEBUG] %s", s), color))
+		tm.Flush()
+	}
 }
 
 func DebugStringln(debugMode bool, s string, color int) {
-	tm.Println(tm.Color(fmt.Sprintf("[DEBUG] %s", s), color))
-	tm.Flush()
+	if debugMode {
+		tm.Println(tm.Color(fmt.Sprintf("[DEBUG] %s", s), color))
+		tm.Flush()
+	}
 }
